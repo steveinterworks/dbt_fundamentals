64982eecf51575d896ff4507ee50f652c94f277f
steve.taylor@interworks.co.uk <steve.taylor@interworks.co.uk>
2024-01-05 14:56:51 +0000

    all edits from quickstart

--- models/customers.sql
+++ models/customers.sql
@@ -6,24 +6,13 @@
 
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from st_raw.jaffle_shop.customers
+    select * from {{ ref('stg_customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from st_raw.jaffle_shop.orders
+    select * from {{ ref('stg_orders') }}
 
 ),
 
